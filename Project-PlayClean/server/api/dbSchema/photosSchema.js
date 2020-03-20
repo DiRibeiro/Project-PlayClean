ce4e415039cde4a5208982c0ac94bf51bfd1218d
DiRibeiro <diribeiro740@gmail.com>
2020-03-20 18:15:31 -0300

    Configure repository

--- Project-PlayClean/server/api/dbSchema/photosSchema.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose')
-
-const photosSchema = new mongoose.Schema({
-    albums: {
-        image: [String]
-    }
-})
-
-module.exports = photosSchema
--- Project-PlayClean/web/src/containers/LineSearchFilter.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-
-export default props => (
-    <li className={ props.liDropmenu ? "liDropmenu" : "" } >
-        <input type="checkbox" id={ `checkbox${ props.id }` } value={ props.label } />
-        <label className={ props.itemDropmenu ? "itemDropmenu" : "" } htmlFor={ `checkbox${ props.id }` }>{ props.label }</label>
-    </li>
-)
