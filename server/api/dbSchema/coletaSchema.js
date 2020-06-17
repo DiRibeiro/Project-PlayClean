47ee80f6c3294ef5622a4c794fcf579e7af3a966
DiRibeiro <diribeiro740@gmail.com>
2020-06-17 18:52:27 -0300

    Reconfigure coletaSchema

--- server/api/dbSchema/coletaSchema.js
+++ server/api/dbSchema/coletaSchema.js
@@ -2,11 +2,9 @@ const mongoose = require('mongoose')
 
 const coletaSchema = new mongoose.Schema({
     whoCreated: mongoose.Schema.Types.ObjectId,
-    districts: [{
-        name: String,
-        selective: String,
-        organic: String
-    }]
+    name: String,
+    selective: String,
+    organic: String
 });
 
 module.exports = coletaSchema
