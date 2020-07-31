071a328b4e0c540cdd3c715d2b256016413fd1f7
Vinicius Fritzen Machado <fritzenvinicius@gmail.com>
2020-07-31 18:11:24 -0300

    Fix Date Values on frontend

--- web/src/helper/date.js
+++ web/src/helper/date.js
@@ -1,13 +1,14 @@
 const months = ["janeiro", "fevereiro", "março", "abril", "maio", "junho", "julho", "agosto", "setembro", "outubro", "novembro", "dezembro"]
-const days = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31]
 
 export const fullDate = value => {
-    let date = new Date(value)
-    return `${ days[date.getUTCDate()]} de ${ months[date.getUTCMonth()]} de ${ date.getUTCFullYear() }`
+    console.log(value)
+    let date = new Date(new Date(value).getTime() - 3*3600*1000)
+   
+    return `${ date.getUTCDate()} de ${ months[date.getUTCMonth()]} de ${ date.getUTCFullYear() }`
 }
 
 export const shortDate = value => {
-    let date = new Date(value)
+    let date = new Date(new Date(value).getTime() - 3*3600*1000)
     return `${ date.getUTCDate()}/${ date.getUTCMonth() + 1 }/${ date.getFullYear() }`
 }
 
